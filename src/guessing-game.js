f62370a550d82af10f3b3388fa2041d0574a7bd5
Maria Kurakevich <marfysa14@gmail.com>
2021-08-10 23:49:37 +0300

    feat: add solut

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.guess_num;
+     }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+   
     guess() {
-
-    }
+        this.guess_num = Math.round((this.max + this.min) / 2);
+        return this.guess_num;
+    }     
+        
+    
 
     lower() {
-
+        this.max = this.guess_num;
     }
 
     greater() {
-
+        this.min = this.guess_num;
     }
 }
 
